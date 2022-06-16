20ba099cf49ada35d262482ff6c822df49a12355
Khayman King <khaymanak@gmail.com>
2022-06-16 16:56:10 -0700

    movie delete test now passes

--- __tests__/movies.test.js
+++ __tests__/movies.test.js
@@ -15,7 +15,7 @@ describe('movie routes', () => {
       return{
         id: movie.id,
         title: movie.title,
-        release: movie.release,
+        release_year: movie.release_year,
         director: movie.director
       };
     });
@@ -29,23 +29,42 @@ describe('movie routes', () => {
       'release_year': expect.any(String),
       'director': expect.any(String)
     });
-    it('should add a new movie', async() => {
-      const res = await request(app)
-        .post('movies')
-        .send({
-          title: 'test',
-          release_year: 2020,
-          director: 'test director'
-        });
-      expect(res.status).toBe(200);
-      expect(res.body).toEqual({
-        'id': expect.anything(),
-        'title': 'test',
-        'release_year': 2020,
-        'director': 'test director'
+  });
+  it('should add a new movie', async() => {
+    const res = await request(app)
+      .post('/movies')
+      .send({
+        title: 'test',
+        release_year: 2020,
+        director: 'test director'
       });
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      'id': expect.anything(),
+      'title': 'test',
+      'release_year': '2020',
+      'director': 'test director'
+    }); 
+  });
+  it('should create and then delete a movie', async() => {
+    const createRes = await request(app)
+      .post('/movies')
+      .send({
+        title: 'test',
+        release_year: 2020,
+        director:'test director'
+      });
+    expect(createRes.status).toBe(200);
+    expect(createRes.body).toEqual({
+      'id': expect.anything(),
+      'title': 'test',
+      'release_year': '2020',
+      'director': 'test director'
     });
-    
+    const delRes = await request(app).delete('/movies/6');
+    expect(delRes.status).toEqual(200);
+    const { body } = await request(app).get('/movies/6');
+    expect(body).toEqual('');
   });
   afterAll(() => {
     pool.end();
