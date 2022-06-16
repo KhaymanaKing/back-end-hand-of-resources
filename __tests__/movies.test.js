e900a64fb7cbd238be4d4f4b07231287bab8c4b2
Khayman King <khaymanak@gmail.com>
2022-06-16 15:19:26 -0700

    added the get all movies test

---
+++ __tests__/movies.test.js
@@ -0,0 +1,24 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const { Movie } = require('../lib/models/Movie');
+
+describe('movie routes'), () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('/movies should return a list of movies', async() => {
+    const res = await request(app).get('/movies');
+    const movies = await Movie.getAll();
+    const expected = movies.map((movie) => {
+      return{
+        id: movie.id,
+        title: movie.title,
+        release: movie.release,
+        director: movie.director
+      };
+    });
+    expect(res.body).toEqual(expected);
+  });
+};
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS beers;
+DROP TABLE IF EXISTS movies;
 
 CREATE TABLE beers (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -10,13 +11,19 @@ CREATE TABLE beers (
     ibu INT,
     pairing VARCHAR    
 );
+CREATE TABLE movies (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR,
+    release_year BIGINT,
+    director VARCHAR
+);
 
 INSERT INTO beers (
- beer_name,
- abv,
- region,
- ibu,
- pairing
+beer_name,
+abv,
+region,
+ibu,
+pairing
 )
 
 VALUES
@@ -25,3 +32,15 @@ VALUES
 ('Voodoo Ranger Imperial IPA', '9', 'Colorado', '70', 'Cheese, Cured Meats'),
 ('Rogue Hazelnut Brown Nectar', '6', 'Oregon', '33', 'Cheese, Turkey'),
 ('Rolling Rock Extra Pale', '5', 'Missouri', '5', 'Nuts, Cured Meats');
+
+INSERT INTO movies (
+title,
+release_year,
+director
+)
+VALUES
+('Army of Darkness', '1992', 'Sam Raimi'),
+('Annihilation', '2018', 'Alex Garland' ),
+('Mortal Kombat','1995', 'Paul W.S. Anderson'),
+('Ip Man', '2008', 'Wilson Yip'),
+('The Ritual', '2017', 'David Bruckner');
